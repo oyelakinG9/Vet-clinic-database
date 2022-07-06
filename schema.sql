f0841a5b0d993b100bdf93d1dd55fc8dd4038267
Macbook <macbook@Macbooks-MacBook.local>
2022-07-06 16:48:29 +0100

    Modify schema sql file

--- schema.sql
+++ schema.sql
@@ -1,9 +1,11 @@
 /* Database schema to keep the structure of entire database. */
-CREATE DATABASE vet_clinic;CREATE TABLE animals (
+CREATE DATABASE vet_clinic;
+  CREATE TABLE animals (
     id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name VARCHAR NOT NULL,
     date_of_birth DATE,
     escape_attempts INT,
     neutered BOOLEAN,
-    weight_kg DECIMAL
-);
+    weight_kg DECIMAL,
+    species VARCHAR NOT NULL
+  );
